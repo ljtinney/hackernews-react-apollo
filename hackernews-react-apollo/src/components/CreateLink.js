09a2f063bd8a3df0be446a14cd135cf9615cdff0
Liam Tinney <ltinney@powerhrg.com>
2020-08-21 13:40:00 -0400

    Update CreateLink.js button

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react'
 
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
 class CreateLink extends Component {
   state = {
     description: '',
@@ -26,7 +37,13 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <button onClick={`... you'll implement this 🔜`}>Submit</button>
+        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+  {() => (
+    <button onClick={`... you'll implement this 🔜`}>
+      Submit
+    </button>
+  )}
+</Mutation>
       </div>
     )
   }
