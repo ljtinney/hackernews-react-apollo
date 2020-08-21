ff3bd88232c98782b8a3ac4b399854dad5f809c4
Liam Tinney <ltinney@powerhrg.com>
2020-08-21 18:22:26 -0400

    Update Header.js

--- hackernews-react-apollo/src/components/Header.js
+++ hackernews-react-apollo/src/components/Header.js
@@ -10,19 +10,23 @@ class Header extends Component {
     return (
       <div className="flex pa1 justify-between nowrap orange">
         <div className="flex flex-fixed black">
-          <div className="fw7 mr1">Hacker News</div>
-          <Link to="/" className="ml1 no-underline black">
-            new
-          </Link>
-          {authToken && (
-            <div className="flex">
-              <div className="ml1">|</div>
-              <Link to="/create" className="ml1 no-underline black">
-                submit
-              </Link>
-            </div>
-          )}
-        </div>
+  <div className="fw7 mr1">Hacker News</div>
+  <Link to="/" className="ml1 no-underline black">
+    new
+  </Link>
+  <div className="ml1">|</div>
+  <Link to="/search" className="ml1 no-underline black">
+    search
+  </Link>
+  {authToken && (
+    <div className="flex">
+      <div className="ml1">|</div>
+      <Link to="/create" className="ml1 no-underline black">
+        submit
+      </Link>
+    </div>
+  )}
+</div>
         <div className="flex flex-fixed">
           {authToken ? (
             <div
