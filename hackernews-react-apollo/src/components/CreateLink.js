55597ca42e884741484897fac8040012d33467ba
Liam Tinney <ltinney@powerhrg.com>
2020-08-21 13:41:52 -0400

    Update CreateLink.js mutation situation

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -38,12 +40,8 @@ class CreateLink extends Component {
           />
         </div>
         <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
-  {() => (
-    <button onClick={`... you'll implement this 🔜`}>
-      Submit
-    </button>
-  )}
-</Mutation>
+        {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
       </div>
     )
   }
