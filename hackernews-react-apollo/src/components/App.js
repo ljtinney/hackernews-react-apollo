4480ad9b18c5c5146bafdcd754a87751046c3338
Liam Tinney <ltinney@powerhrg.com>
2020-08-21 14:46:19 -0400

    Update render for App.js

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -14,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
